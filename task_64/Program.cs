043100f0e7b7c598005e362648734247b4e8a9e6
Сучков Алексей <AlexiySV@yandex.ru>
2022-11-16 11:39:17 +0300

    task_64

---
+++ task_64/Program.cs
@@ -0,0 +1,26 @@
+﻿int InputData(string message)
+{
+    Console.Write(message);
+    int d = int.Parse(Console.ReadLine());
+    return d;
+}
+
+int AllNatur(int n)
+{
+    if (n > 1) return AllNatur(n - 1) + 1;
+    else return 1;
+}
+
+void Print(int n)
+{
+    for (int i = n; i > 0; i--)
+    {
+        Console.Write(AllNatur(i) + " ");
+    }
+}
+
+int n = InputData("Введите N ");
+
+Print(n);
+
+
