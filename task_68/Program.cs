e16efffd8641ad399268f3235f872d1286faffb4
Сучков Алексей <AlexiySV@yandex.ru>
2022-11-16 13:34:46 +0300

    task_68

---
+++ task_68/Program.cs
@@ -0,0 +1,19 @@
+﻿long InputData(string msg)
+{
+    Console.Write(msg);
+    long d = long.Parse(Console.ReadLine());
+    return d;
+}
+
+long AckermansFunction(long m, long n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AckermansFunction(m - 1, 1);
+    else return AckermansFunction(m - 1, AckermansFunction(m, n - 1)); ;
+}
+
+long m = InputData("Введите M ");
+long n = InputData("Введите N ");
+
+if (m >= 0 && n >= 0) Console.WriteLine(AckermansFunction(m, n));
+else Console.WriteLine("Ошибка");
