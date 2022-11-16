4b7d109a2ade0e2238b95535afbb0e5bb9afda14
Сучков Алексей <AlexiySV@yandex.ru>
2022-11-16 12:21:33 +0300

    task_66

---
+++ task_66/Program.cs
@@ -0,0 +1,17 @@
+﻿int InputData(string msg)
+{
+    Console.Write(msg);
+    int d = int.Parse(Console.ReadLine());
+    return d;
+}
+
+int RecSum(int m, int n)
+{
+    if (m <= n)  return  RecSum(m, n - 1) + n;
+    else return 0;
+}
+
+int m = InputData("Введите M ");
+int n = InputData("Введите N ");
+if (m < n) Console.WriteLine(RecSum(m, n));
+else Console.WriteLine("Ошибка");
